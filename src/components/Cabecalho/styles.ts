c24dca779595ad40dcfc582a6cc7b4034321bbc3
Gustavo Lage <gustavo.lage.dev@gmail.com>
2025-09-29 12:34:58 -0300

    conversao para styles, cabecalho

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Cabecalho = styled.div`
+  background-color: ${cores.corSecundaria};
+  color: ${cores.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
